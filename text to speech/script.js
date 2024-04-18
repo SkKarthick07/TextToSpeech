6daaa34b4916f32055178a33866ef4d95f83b794
Karthick S <147124196+SkKarthick07@users.noreply.github.com>
2024-04-18 14:55:51 +0530

    Delete text to speech directory

--- text to speech/script.js
+++
@@ -1,6 +0,0 @@
-function speak() {
-    const textToSpeak = document.getElementById("textToSpeak").value;
-    const speech = new SpeechSynthesisUtterance(textToSpeak);
-    //this line tells the browser to speak the text stored in the speech object
-    speechSynthesis.speak(speech);
-}
